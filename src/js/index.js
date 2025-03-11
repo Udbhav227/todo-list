7c2c8f64a9a16bdc2f362888bc3da8a42d86abed
Udbhav <udbhav227@gmail.com>
2025-03-11 19:03:01 +0530

    Refactor sidebar layout and enhance button functionality with icons

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
 import reset from './../styles/reset.css';
 import main from './../styles/main.css';
+import variables from './../styles/variables.css';
+import starIcon from './../assets/icons/star.svg';   
+import todayIcon from './../assets/icons/today.svg';
+import calendarIcon from './../assets/icons/week.svg';
+import importantIcon from './../assets/icons/imp.svg';
+import taskDoneIcon from './../assets/icons/task-done.svg';
+import addIcon from './../assets/icons/add.svg';
 
-console.log('Working!!')
+
+console.log('Working!!');
